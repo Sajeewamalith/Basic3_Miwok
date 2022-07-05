73d6f5e13a92aae5dd231ecb8919ca5c67a3fc8d
sajee <sajeewamalith0219@gmail.com>
2022-07-05 22:46:17 +0530

    modify array to arrayList

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
@@ -13,19 +15,21 @@ public class NumbersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_numbers);
 
         // Create an array of words
-        String[] words = new String[10];
-        words[0] = "one";
-        words[1] = "two";
-        words[2] = "three";
-        words[3] = "four";
-        words[4] = "five";
-        words[5] = "six";
-        words[6] = "seven";
-        words[7] = "eight";
-        words[8] = "nine";
-        words[9] = "ten";
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("Two");
+        words.add("Three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
 
-        Log.v("NumbersActivity","Word at index 0: "+ words[0]);
-        Log.v("NumbersActivity","Word at index 1: "+ words[1]);
+
+
+        Log.v("NumbersActivity","Word at index 0: "+ words.get(0));
+        Log.v("NumbersActivity","Word at index 1: "+ words.get(1));
     }
 }
